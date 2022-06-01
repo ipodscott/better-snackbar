27ccd0c6ca05ded2edd1ba8846d1c7e342d7577e
scott <scottsaunders@mac.com>
2022-05-31 23:11:00 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+Plugin Name: Better Snack bar
+Plugin URI: https://scottsaunders.design
+Description: Moves the snackbar to the top right vs. the bottom left.
+Version: 1.0
+Author: Scott Saunders
+Author URI: https://scottsaunders.design
+License: GPLv2 or later
+Text Domain: scottsaunders.design
+*/
+
+function snackbar_stuff()
+{ 
+  wp_enqueue_style( 'admin', plugin_dir_url( __FILE__ ) . 'snackbar.css',true,'1.1','all');
+}
+add_action('admin_footer', 'snackbar_stuff');
